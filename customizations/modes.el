6b3ca60454e62cdcfb04841a7c80894c2247b16e
pjaspers <junkiesxl@gmail.com>
2012-07-29 18:02:38 +0200

    Removing legacy references.

--- customizations/modes.el
+++ customizations/modes.el
@@ -36,7 +36,6 @@
 (setq auto-mode-alist (cons '("\\.erb" . nxml-mode) auto-mode-alist))
 
 ; ruby
-(vendor 'ruby-hacks)
 (setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("Capfile" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.rake" . ruby-mode) auto-mode-alist))
