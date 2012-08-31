d5250f48d15340a9a360e4b14fc51cbfdf0f3fd1
pjaspers <junkiesxl@gmail.com>
2012-08-31 15:10:21 +0200

    Adding melpa mode.

--- customizations/packages.el
+++ customizations/packages.el
@@ -1,6 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
 (when (not package-archive-contents)
