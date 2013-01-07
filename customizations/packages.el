9c69dc3528f56c5d7efc48a2547135d2d02b13e1
pjaspers <junkiesxl@gmail.com>
2013-01-07 11:48:29 +0100

    MOAR MODES

--- customizations/packages.el
+++ customizations/packages.el
@@ -7,12 +7,14 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(setq ruby-packages '(rvm inf-ruby yaml-mode))
+(setq ruby-packages '(rvm inf-ruby yaml-mode sass-mode))
 (setq js-packages '(js2-mode coffee-mode))
 (setq git-packages '(magit mo-git-blame))
+(setq text-packages '(markdown-mode))
+(setq lisp-packages '(elisp-slime-nav-mode))
 (setq global-packages '(smex textmate ace-jump-mode color-theme-gruber-darker multiple-cursors))
 
-(setq pjaspers-packages (append ruby-packages js-packages git-packages global-packages))
+(setq pjaspers-packages (append ruby-packages js-packages git-packages text-packages lisp-packages global-packages))
 
 (dolist (p pjaspers-packages)
   (when (not (package-installed-p p))
