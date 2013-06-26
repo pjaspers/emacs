10885e0448ff2fad0687ec75a34d4c8b267730a1
pjaspers <junkiesxl@gmail.com>
2013-06-26 16:33:34 +0200

    Using org-agenda to get ALL THE TODOS.

--- customizations/org-mode.el
+++ customizations/org-mode.el
@@ -1,3 +1,22 @@
 (setq org-directory "~/Dropbox/Documents/Personal/notes")
 (setq org-default-notes-file (concat org-directory "/notes.org"))
+
 (define-key global-map "\C-cc" 'org-capture)
+(define-key global-map "\C-ca" 'org-agenda)
+
+
+(setq org-refile-targets '((nil :maxlevel . 2)
+                                ; all top-level headlines in the
+                                ; current buffer are used (first) as a
+                                ; refile target
+                           (org-agenda-files :maxlevel . 2)))
+
+;; provide refile targets as paths, including the file name
+;; (without directory) as level 1 of the path
+(setq org-refile-use-outline-path 'file)
+
+;; allow to create new nodes (must be confirmed by the user) as
+;; refile targets
+(setq org-refile-allow-creating-parent-nodes 'confirm)
+
+(setq org-agenda-files '("~/Dropbox/Documents/Personal/notes"))
