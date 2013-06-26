df5ad9f764ca779b3e9cd7cbe7e02304ebd8fd5d
pjaspers <junkiesxl@gmail.com>
2013-06-26 16:32:26 +0200

    Adding auto-complete mode.

--- customizations/packages.el
+++ customizations/packages.el
@@ -1,7 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -9,10 +9,11 @@
 
 (setq ruby-packages '(rvm inf-ruby yaml-mode sass-mode))
 (setq js-packages '(js2-mode coffee-mode))
-(setq git-packages '(magit mo-git-blame))
+(setq ruby-packages '(robe))
+(setq git-packages '(magit mo-git-blame git-commit-mode gitconfig-mode gitignore-mode))
 (setq text-packages '(markdown-mode))
-(setq lisp-packages '(elisp-slime-nav-mode))
-(setq global-packages '(smex textmate ace-jump-mode color-theme-gruber-darker multiple-cursors))
+(setq lisp-packages '())
+(setq global-packages '(smex textmate ace-jump-mode color-theme-gruber-darker multiple-cursors zenburn-theme browse-kill-ring auto-complete))
 
 (setq pjaspers-packages (append ruby-packages js-packages git-packages text-packages lisp-packages global-packages))
 
