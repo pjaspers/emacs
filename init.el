b9c049c09b58d91447973b25d06d115c87927a62
pjaspers <junkiesxl@gmail.com>
2014-01-10 12:17:36 +0100

    More explicit in load paths (emacs 24.4 complained)

--- init.el
+++ init.el
@@ -1,15 +1,16 @@
-(add-to-list 'load-path "~/.emacs.d/")
-
 ;; custom place to save, wait for it, customizations
 (setq custom-file "~/.emacs.d/custom.el")
 (when (file-exists-p "custom.el") (load "custom"))
 
-(load "customizations/packages") ;; Fetch modes
-(load "customizations/ui")       ;; Set up all things colors/fonts/appearance
-(load "customizations/bindings") ;; All non platform specific bindings
-(load "customizations/mac")      ;; All things related to the mac
-(load "customizations/modes")    ;; Setup mode specific stuff
-(load "customizations/shell")    ;; All things with paths and shells
-(load "customizations/global")	 ;; All things with paths and shells
-(load "customizations/org-mode") ;; Trying to get back in the org-mode habit.
-(load "elisp/defuns")            ;; Lisp helper functions
+(add-to-list 'load-path "~/.emacs.d/customizations")
+(load "packages") ;; Fetch modes
+(load "ui")       ;; Set up all things colors/fonts/appearance
+(load "bindings") ;; All non platform specific bindings
+(load "mac")      ;; All things related to the mac
+(load "modes")    ;; Setup mode specific stuff
+(load "shell_custom")    ;; All things with paths and shells
+(load "global")	 ;; All things with paths and shells
+(load "org-mode") ;; Trying to get back in the org-mode habit.
+
+(add-to-list 'load-path "~/.emacs.d/elisp")
+(load "defuns")            ;; Lisp helper functions
