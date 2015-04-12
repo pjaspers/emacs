bd8f405947a0d3b0e6e83a2d2e4386f82b68706a
pjaspers <piet@pjaspers.com>
2015-04-12 12:32:17 +0200

    Now using cask to manage all packages.
    Main reason was melpa had troubles keeping its versions straight. Or
    rather a dependency wasn't being upgraded which caused the parent mode
    to stop working.
    
    Cask seems more like bundler

---
+++ Cask
@@ -0,0 +1,58 @@
+(source gnu)
+(source melpa)
+
+;; Ruby packages
+(depends-on "chruby")
+(depends-on "inf-ruby")
+(depends-on "yaml-mode")
+(depends-on "sass-mode")
+(depends-on "robe")
+(depends-on "web-mode")
+(depends-on "rubocop")
+(depends-on "projectile-rails")
+
+;; JS packages
+(depends-on "js3-mode")
+(depends-on "coffee-mode")
+
+;; Git packages
+(depends-on "magit")
+(depends-on "mo-git-blame")
+(depends-on "git-commit-mode")
+(depends-on "gitconfig-mode")
+(depends-on "gitignore-mode")
+
+;; Global packages
+(depends-on "smex")
+(depends-on "textmate")
+(depends-on "ace-jump-mode")
+(depends-on "multiple-cursors")
+(depends-on "zenburn-theme")
+(depends-on "browse-kill-ring")
+(depends-on "auto-complete")
+(depends-on "projectile")
+(depends-on "flx-ido")
+(depends-on "flycheck")
+(depends-on "expand-region")
+(depends-on "exec-path-from-shell")
+(depends-on "swift-mode")
+(depends-on "ag")
+(depends-on "fill-column-indicator")
+
+;; Color themes
+(depends-on "color-theme-sanityinc-tomorrow")
+
+(depends-on "flycheck-cask")
+
+(depends-on "cask")
+(depends-on "dash")
+(depends-on "f")
+(depends-on "htmlize")
+(depends-on "idle-highlight-mode")
+
+(depends-on "nyan-mode")
+(depends-on "popwin")
+(depends-on "s")
+(depends-on "smartparens")
+(depends-on "use-package")
+(depends-on "yasnippet")
--- customizations/packages.el
+++ customizations/packages.el
@@ -1,30 +1,4 @@
-(require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
-(package-initialize)
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-(setq ruby-packages '(chruby inf-ruby yaml-mode sass-mode robe web-mode rubocop))
-(setq js-packages '(js2-mode coffee-mode))
-(setq git-packages '(magit mo-git-blame git-commit-mode gitconfig-mode gitignore-mode))
-(setq text-packages '(markdown-mode))
-(setq lisp-packages '())
-(setq computering-packages '(go-mode))
-(setq color-themes '(color-theme-sanityinc-tomorrow color-theme-gruber-darker))
-(setq global-packages '(smex textmate ace-jump-mode multiple-cursors zenburn-theme browse-kill-ring auto-complete projectile flx-ido flycheck expand-region exec-path-from-shell swift-mode ag fill-column-indicator ))
-
-(setq pjaspers-packages (append ruby-packages js-packages git-packages text-packages lisp-packages global-packages color-themes computering-packages))
-
-(dolist (p pjaspers-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
-
-
-;; No package found.
-;; More [here](https://github.com/pry/pry/wiki/ruby-dev.el)
-(add-to-list 'load-path "~/.emacs.d/ruby-dev.el" )
-(autoload 'turn-on-ruby-dev "ruby-dev" nil t)
-(add-hook 'ruby-mode-hook 'turn-on-ruby-dev)
+;; Let's use [cask](https://github.com/cask/cask)
+;; All packages now defined in `Cask`
+(require 'cask "/usr/local/share/emacs/site-lisp/cask.el")
+(cask-initialize)
