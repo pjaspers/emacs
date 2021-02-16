5d8d4a684d85a15408baf5a72d710da20a39c697
pjaspers <piet@pjaspers.com>
2021-02-16 10:04:40 +0100

    Make config starting directory aware
    As someone pointed out, emacs can load from ~/.config/emacs now, so no
    sense in keeping it around in the ~

--- init.el
+++ init.el
@@ -1,12 +1,38 @@
-;; custom place to save, wait for it, customizations
+;; This emacs won't start itself, let's dig in.
+;;
+;;
+;; Make sure that no matter where this config is stored, much like the
+;; Iron Giant, it can always find its parts.
 
-(setq custom-file "~/.emacs.d/custom.el")
-(when (file-exists-p "custom.el") (load "custom"))
-(add-to-list 'load-path "~/.emacs.d/customizations")
+(defvar pjaspers-start-directory
+  user-emacs-directory
+  "Directory where emacs loaded from")
+
+(defconst pjaspers-customizations-directory
+  (expand-file-name (concat pjaspers-start-directory "customizations/"))
+  "Has most of the emacs config")
+
+(defconst pjaspers-elisp-directory
+  (expand-file-name (concat pjaspers-start-directory "elisp/"))
+  "Has the lisp functions")
+
+(defconst pjaspers-custom-file
+  (expand-file-name (concat pjaspers-start-directory "custom.el"))
+  "Such custom")
+
+(defconst pjaspers-scratch-file
+  (expand-file-name (concat pjaspers-start-directory "scratch.el"))
+  "I have a bad habit of stuffing everything in that one")
+
+;; Load custom.el if any
+(setq custom-file pjaspers-custom-file)
+(when (file-exists-p custom-file) (load "custom"))
 
 ;; Homebrew dir
 (let ((default-directory "/usr/local/share/emacs/site-lisp/"))
   (normal-top-level-add-subdirs-to-load-path))
+
+(add-to-list 'load-path pjaspers-customizations-directory)
 
 (require 'pjaspers-packages)
 
@@ -37,6 +63,6 @@
 (setenv "LANG" "en_BE.UTF-8")
 (setenv "LC_ALL" "en_BE.UTF-8")
 
-(add-to-list 'load-path "~/.emacs.d/elisp")
+(add-to-list 'load-path pjaspers-elisp-directory)
 (load "defuns")            ;; Lisp helper functions
 (put 'narrow-to-region 'disabled nil)
